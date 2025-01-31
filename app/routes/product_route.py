b52558c0075f20c53890659e357af97d68b26bb9
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2025-01-31 16:25:38 -0300

    feat: search products by description

--- app/routes/product_route.py
+++ app/routes/product_route.py
@@ -12,3 +12,7 @@ async def create_product(product: ProductRequest):
     product_service.create_product(product.dict())
 
     return product
+
+@router.get("/")
+async def search_products(description: str):
+    return product_service.search_product_by_description(description)
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,6 +1,8 @@
 from ..indexes.products import create_products_index
 from ..connection import create_redis_connection
 
+from redisearch import Query, Client
+
 import random
 
 class ProductService:
@@ -9,7 +11,12 @@ class ProductService:
         self.product_index = create_products_index(self.redis_connection)
 
     def create_product(self, product: dict):
-        return self.product_index.add_document(self.generateRandomCode(), **product)
+        return self.product_index.add_document(self.generate_random_code(), **product)
 
-    def generateRandomCode(self):
-        return random.randint(100000, 999999)
+    def search_product_by_description(self, description: str):
+        query_string = f'@description:({description}*)'
+        query = Query(query_string).verbatim()
+        return self.product_index.search(query)
+
+    def generate_random_code(self):
+        return str(random.randint(100000, 999999))
